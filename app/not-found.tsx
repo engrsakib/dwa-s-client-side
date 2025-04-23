0e46c731c362f61be90d32d640c2da4dd5bc63f6
engrsakib <snazmussakib01@gmail.com>
2025-04-23 15:21:48 +0600

    not found page define for globally but design architect is not properly done

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center'>This pages is not found</h1>
+        </div>
+    );
+};
+
+export default notFound;
